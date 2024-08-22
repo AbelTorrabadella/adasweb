80f8614f5e0a423497d188dd856bf90aeffa263e
AbelTv <atorrabadella10@xarxa.fedac.cat>
2024-08-22 13:48:32 +0200

    prova

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -108,7 +108,9 @@ function Menu(props) {
     if (props.isPortada === true && windowSize > 1000) { //SI ES LA PORTADA PER UNA PANTALLA NORMAL SERÀ 21
       setAlturaMenu("21%")
     }
-
+    if (scrollPosition > 0 && windowSize < 1000) {
+      setAlturaMenu("12%")
+    }
     
   })
   return (
